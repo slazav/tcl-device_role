417fdcff97a91c335cb0e365b9583345e277ffbe
Vladislav Zavjalov <slazav@altlinux.org>
2020-10-01 15:19:07 +0100

    r_gauge: add Pfeiffer Adixen ASM340 leak detector

--- r_gauge.tcl
+++ r_gauge.tcl
@@ -970,4 +970,39 @@ itcl::class leak_ag_vs {
 }
 
 ######################################################################
+# Pfeiffer Adixen ASM340 leak detector
+#
+
+itcl::class leak_asm340 {
+  inherit interface
+  proc test_id {id} {
+    if {$id == {Adixen ASM340 leak detector}} {return 1}
+  }
+
+  variable chan;  # channel to use
+
+  constructor {d ch id} {
+    # channels are not supported now
+    set valnames [list "Leak" "Pin"]
+    set dev $d
+  }
+
+  # convert numbers received from the leak detector:
+  # 123+1 -> 1.23e+03
+  method conv_number {v} {
+    if { [regexp {^([0-9]+)([\+\-][0-9]+)$} $v a b c] } { set v "${b}e${c}" }
+    return [format "%.2e" $v]
+  }
+
+  ############################
+  method get {} {
+    # inlet pressure, measurement (calibrated)
+    set pin   [conv_number [$dev cmd ?PE]]
+    set leak  [conv_number [$dev cmd ?LE2]]
+    return [list $leak $pin]
+  }
+  method get_auto {} { return [get] }
+}
+
+######################################################################
 } # namespace
