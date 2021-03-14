58ec089afba62e5390170f8fee1d5e833738827e
Vladislav Zavjalov <slazav@altlinux.org>
2021-03-14 12:44:46 +0000

    d_keysight_gen.tcl: fix generator id numbers

--- d_keysight_gen.tcl
+++ d_keysight_gen.tcl
@@ -1,6 +1,17 @@
 ## Common functions for all keysight generators
 ## In inherit statement this class should go before interface class
 ## to override dev variable
+##
+## This class is used in ac_source, dc_source, noise_source, pulse_source roles.
+##
+## id examples:
+## Agilent Technologies,33510B,MY52201807,3.05-1.19-2.00-52-00
+## Agilent Technologies,33522A,MY50005619,2.03-1.19-2.00-52-00
+## Agilent
+## Technologies,33511B,MY52300310,2.03-1.19-2.00-52-00
+
+
+
 itcl::class keysight_gen {
 
   protected variable chan;      # Channel to use (1,2 or empty).
@@ -21,11 +32,11 @@ itcl::class keysight_gen {
     # Set number of channels for this model.
     set model [test_id $id]
     switch -exact -- $model {
+      33220A -
       33509B -
       33511B -
       33520A -
-      33220A -
-      33221A { set nch 1 }
+      33521A { set nch 1 }
       33510B -
       33522A { set nch 2 }
       default { error "keysight_gen::get_nch: unknown model: $model" }
@@ -48,11 +59,11 @@ itcl::class keysight_gen {
   # return model name for known generator id
   proc test_id {id} {
     # 1-channel
+    if {[regexp {,33220A,} $id]} {return {33220A}}
     if {[regexp {,33509B,} $id]} {return {33509B}}
     if {[regexp {,33511B,} $id]} {return {33511B}}
     if {[regexp {,33520A,} $id]} {return {33520A}}
-    if {[regexp {,33520A,} $id]} {return {33220A}}
-    if {[regexp {,33521A,} $id]} {return {33221A}}
+    if {[regexp {,33521A,} $id]} {return {33521A}}
     # 2-channel
     if {[regexp {,33510B,} $id]} {return {33510B}}
     if {[regexp {,33522A,} $id]} {return {33522A}}
